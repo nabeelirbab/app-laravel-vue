3c19396eab79700381c3785723103a483b3bfb06
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-01-24 15:10:17 +0545

    delete unwanted files

--- resources/views/admin/users/partials/verified.blade.php
+++
@@ -1,9 +0,0 @@
-<td>
-	<span class="text-primary">
-			@if(!$user->approved_at)
-            	<i class='fa-2x fa fa-check-circle'></i>
-            @else
-            	<i class="fa-2x fa fa-close-circle"></i>
-            @endif
-    </span>
-</td>
