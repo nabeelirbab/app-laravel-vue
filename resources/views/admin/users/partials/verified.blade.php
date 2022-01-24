af8228f0422c416e1cce0396fa0b76928075c532
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-01-24 12:26:38 +0545

    added file

---
+++ resources/views/admin/users/partials/verified.blade.php
@@ -0,0 +1,9 @@
+<td>
+	<span >
+			@if(!$user->approved_at)
+            	<i class="fas fa-check"></i>
+            @else
+            	<i class="fas fa-close"></i>
+            @endif
+    </span>
+</td>
