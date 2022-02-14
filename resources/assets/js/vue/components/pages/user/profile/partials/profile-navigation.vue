733cef1e8022ea5429ae04026edd238c771d685d
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-02-14 20:27:58 +0545

    remove from filter tabs

--- resources/assets/js/vue/components/pages/user/profile/partials/profile-navigation.vue
+++ resources/assets/js/vue/components/pages/user/profile/partials/profile-navigation.vue
@@ -3,7 +3,6 @@
         <ul>
             <li><router-link :to="{ name: 'profile_all' }">All</router-link></li>
             <li><router-link :to="{ name: 'profile_music' }">Music</router-link></li>
-            <li><router-link :to="{ name: 'profile_releases' }">Releases</router-link></li>
             <li v-if="$can('add events')"><router-link :to="{ name: 'profile_events' }">Events</router-link></li>
             <li><router-link :to="{ name: 'profile_videos' }">Videos</router-link></li>
             <li v-if="$can('add merch')"><router-link :to="{ name: 'profile_merch' }">Merch</router-link></li>
