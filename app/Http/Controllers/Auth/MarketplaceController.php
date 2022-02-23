06e4424d851dd885ebad2d184bdc38567865bc90
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-02-23 14:49:47 +0545

    add exception

--- app/Http/Controllers/Auth/MarketplaceController.php
+++ app/Http/Controllers/Auth/MarketplaceController.php
@@ -10,15 +10,20 @@ class MarketplaceController extends Controller
 {
     public function store(Request $request)
     {
-        $user = User::find($request->user_id);
+        try{
+            $user = User::find($request->user_id);
 
-        $user->createAccount($request);
-        if($request->phone) {
-            $user->phone = $request->phone;
-            $user->save();
+            $user->createAccount($request);
+            if($request->phone) {
+                $user->phone = $request->phone;
+                $user->save();
+            }
+
+            return $user;
+        } catch(Exception $e) {
+            return ['status' => 'error', 'message' => $e->getMessage(), 'code' => $e->getCode() ];
         }
-
-        return $user;
+        
     }
 
     public function update(Request $request)
