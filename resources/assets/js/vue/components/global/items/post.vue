294fc0729cb3c27470e9f0afe35c191cbbb2086b
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-03-10 14:10:39 +0545

    updates

--- resources/assets/js/vue/components/global/items/post.vue
+++ resources/assets/js/vue/components/global/items/post.vue
@@ -14,15 +14,14 @@
                     {{ postBody }}
                 </div>
                 <div class="p-post-text" v-if="post.attachment">
-                    <router-link
-                        :to="post.attachment.files.medium.url" target= '_blank' >
+                    <a href="post.attachment.files.medium.url" target="_blank">
                         <img
                             v-if="post.attachment"
                             :src="post.attachment.files.medium.url"
                             :alt="post.attachment.alt"
                             class="p-post-image"
                         />
-                    </router-link>
+                    </a>
                 </div>
             </div>
             <div class="p-item-meta">
