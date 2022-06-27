e1c3fd7752e123e861ee96a315ca8415764c911a
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-06-27 12:40:41 +0545

    testing video upload

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -54,6 +54,8 @@ class VideoController extends Controller
         }
         // receive the file
         $save = $receiver->receive();
+
+        print_r($save);
         // check if the upload has finished (in chunk mode it will send smaller files)
         if ($save->isFinished()) {
             // save the file and return any response you need
