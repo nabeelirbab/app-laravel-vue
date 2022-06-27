ceaade8fdaf35870acb87cdf7e56260f856b1bc1
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-06-27 15:20:37 +0545

    updated

--- resources/assets/js/vue/components/modals/upload-video/uploader.vue
+++ resources/assets/js/vue/components/modals/upload-video/uploader.vue
@@ -29,7 +29,7 @@
                 testChunks: false,
                 maxFileSize: 500 * 1024 * 1024,
                 chunkSize: 1 * 1024 * 1024, // 1MB
-                simultaneousUploads: 2,
+                simultaneousUploads: 10,
                 forceChunkSize: true,
                 headers: {
                     'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content,
