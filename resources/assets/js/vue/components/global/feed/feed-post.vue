5a5288b78b1c21c047eba22b1a0af6fd619e5871
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-11-13 15:09:34 +0545

    testing

--- resources/assets/js/vue/components/global/feed/feed-post.vue
+++ resources/assets/js/vue/components/global/feed/feed-post.vue
@@ -2,7 +2,7 @@
     <div :class="'masonry-item ' + item.component">
         <!-- discovery-post -->
         <div class="masonry-inner masonry-inner-fixed">
-            {{item.action_id}}
+            {{item}}
             <post  :post="item" :id="item.action_id" />
             
         </div>
