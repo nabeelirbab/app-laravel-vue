74ba4393a0eb96e955c54151b0d92ce2d8ab5014
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-11-18 10:04:20 +0545

    updates

--- resources/assets/js/vue/components/pages/user/profile/partials/profile-navigation.vue
+++ resources/assets/js/vue/components/pages/user/profile/partials/profile-navigation.vue
@@ -1,32 +1,33 @@
 <template>
     <nav class="sub-nav">
         <ul>
-            <li><a href="#" v-bind:class="[currentRouteName == 'profile_all' ? 'router-link-exact-active router-link-active' : '']"  @click="changeRoute('profile_all')">All</a></li>
-            <li><a href="#" v-bind:class="[currentRouteName == 'profile_music' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_music')">Music</a></li>
-            <li ><a href="#" v-bind:class="[currentRouteName == 'profile_events' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_events')">Events</a></li>
-            <li><a href="#"  v-bind:class="[currentRouteName == 'profile_videos' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_videos')">Videos</a></li>
-            <li ><a href="#" v-bind:class="[currentRouteName == 'profile_merch' ? 'router-link-exact-active router-link-active' : '']"  @click="changeRoute('profile_merch')">Merch</a></li>
-            <li><a href="#" v-bind:class="[currentRouteName == 'profile_posts' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_posts')">Posts</a></li>
+            <li><a href="#" v-bind:class="[routename == 'profile_all' ? 'router-link-exact-active router-link-active' : '']"  @click="changeRoute('profile_all')">All</a></li>
+            <li><a href="#" v-bind:class="[routename == 'profile_music' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_music')">Music</a></li>
+            <li ><a href="#" v-bind:class="[routename == 'profile_events' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_events')">Events</a></li>
+            <li><a href="#"  v-bind:class="[routename == 'profile_videos' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_videos')">Videos</a></li>
+            <li ><a href="#" v-bind:class="[routename == 'profile_merch' ? 'router-link-exact-active router-link-active' : '']"  @click="changeRoute('profile_merch')">Merch</a></li>
+            <li><a href="#" v-bind:class="[routename == 'profile_posts' ? 'router-link-exact-active router-link-active' : '']" @click="changeRoute('profile_posts')">Posts</a></li>
         </ul>
     </nav>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            routename: this.$route.name
+        };
+    },
     props: {
         user: {
             type: Object,
             required: true,
         }
     },
-    computed: {
-        currentRouteName() {
-            return this.$route.name;
-        }
-    },
     methods: {
         changeRoute(pathname) {
             //this.$router.push({ path: pathname });
+            this.routename = pathname;
             this.$emit("linkclick");
         },
         
