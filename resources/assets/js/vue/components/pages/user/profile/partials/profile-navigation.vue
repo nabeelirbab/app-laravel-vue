281fe75d557145f8ce081dfe94fa098915dc24b5
Basant Raj <basantaraj.kunyo@my.offgamers.com>
2022-11-18 09:31:43 +0545

    updates

--- resources/assets/js/vue/components/pages/user/profile/partials/profile-navigation.vue
+++ resources/assets/js/vue/components/pages/user/profile/partials/profile-navigation.vue
@@ -1,12 +1,12 @@
 <template>
     <nav class="sub-nav">
         <ul>
-            <li><router-link :to="{ name: 'profile_all' }" @click.native="changeRoute">All</router-link></li>
-            <li><router-link :to="{ name: 'profile_music' }" @click.native="changeRoute">Music</router-link></li>
-            <li ><router-link :to="{ name: 'profile_events' }" @click.native="changeRoute">Events</router-link></li>
-            <li><router-link :to="{ name: 'profile_videos' }" @click.native="changeRoute">Videos</router-link></li>
-            <li ><router-link :to="{ name: 'profile_merch' }" @click.native="changeRoute">Merch</router-link></li>
-            <li><router-link :to="{ name: 'profile_posts' }" @click.native="changeRoute">Posts</router-link></li>
+            <li><router-link :to="{ name: 'profile_all' }" @click.native="changeRoute('profile_all')">All</router-link></li>
+            <li><router-link :to="{ name: 'profile_music' }" @click.native="changeRoute('profile_music')">Music</router-link></li>
+            <li ><router-link :to="{ name: 'profile_events' }" @click.native="changeRoute('profile_events')">Events</router-link></li>
+            <li><router-link :to="{ name: 'profile_videos' }" @click.native="changeRoute('profile_videos')">Videos</router-link></li>
+            <li ><router-link :to="{ name: 'profile_merch' }" @click.native="changeRoute('profile_merch')">Merch</router-link></li>
+            <li><router-link :to="{ name: 'profile_posts' }" @click.native="changeRoute('profile_posts')">Posts</router-link></li>
         </ul>
     </nav>
 </template>
@@ -20,8 +20,8 @@ export default {
         }
     },
      methods: {
-        changeRoute() {
-            this.$router.push({ path: '/expedition' });
+        changeRoute(pathname) {
+            this.$router.push({ path: pathname });
         },
         
     },
